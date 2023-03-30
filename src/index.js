8c299aec3d93a692bc3cc6b537af6ed716f3cb2e
Danfall369 <Danfall963@gmail.com>
2023-03-30 02:48:28 -0400

    complete all the function to add & remove

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
-// eslint-disable-next-line no-unused-vars
 import _ from 'lodash';
 import './style.css';
 
-const tasks = [
+_();
+
+let tasks = [
   {
     description: 'Buy groceries',
     completed: false,
@@ -27,15 +28,43 @@ function createTaskItem(task) {
   checkbox.type = 'checkbox';
   checkbox.checked = task.completed;
   taskItem.appendChild(checkbox);
-  taskItem.innerHTML += task.description;
+
+  // Create input element
+  const input = document.createElement('input');
+  input.type = 'text';
+  input.value = task.description;
+  input.classList.add('inputList');
+  taskItem.appendChild(input);
+
   if (task.completed) {
     taskItem.classList.add('completed');
   }
+
+  // Add event listener to input element
+  input.addEventListener('blur', (event) => {
+    const newDescription = event.target.value.trim();
+    if (newDescription) {
+      task.description = newDescription;
+
+      // Store tasks in local storage
+      localStorage.setItem('tasks', JSON.stringify(tasks));
+    } else {
+      // If the input is empty, restore the original description
+      event.target.value = task.description;
+    }
+  });
+
   return taskItem;
 }
 
 function populateTodoList() {
   const todoList = document.getElementById('todo-list');
+
+  // Retrieve tasks from local storage
+  const storedTasks = JSON.parse(localStorage.getItem('tasks'));
+  if (storedTasks) {
+    tasks = storedTasks;
+  }
 
   // Sort tasks by index value
   tasks.sort((a, b) => a.index - b.index);
@@ -45,6 +74,39 @@ function populateTodoList() {
     todoList.appendChild(taskItem);
   });
 }
+
+const addItemInput = document.getElementById('addItem');
+const todoList = document.getElementById('todo-list'); // Define todoList globally
+addItemInput.addEventListener('keydown', (event) => {
+  if (event.key === 'Enter') {
+    const newTask = {
+      index: tasks.length,
+      description: addItemInput.value,
+      completed: false,
+    };
+    tasks.push(newTask);
+    const taskItem = createTaskItem(newTask);
+    todoList.appendChild(taskItem); // Use todoList globally
+
+    // Store tasks in local storage
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+
+    addItemInput.value = '';
+  }
+});
+
+const clearButton = document.getElementById('clearButton');
+clearButton.addEventListener('click', () => {
+  const completedItems = todoList.querySelectorAll('li input:checked');
+  completedItems.forEach((item) => {
+    const listItem = item.parentNode;
+    tasks.splice(listItem.dataset.index, 1);
+    listItem.remove();
+  });
+
+  // Store tasks in local storage
+  localStorage.setItem('tasks', JSON.stringify(tasks));
+});
 
 // Call the populateTodoList function when the page finishes loading
 window.addEventListener('load', populateTodoList);

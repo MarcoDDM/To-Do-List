ef5a5c35fbc56e60c80441af19451d73c27a02fa
Danfall369 <Danfall963@gmail.com>
2023-04-01 04:54:01 -0400

    all changes for the review Done!

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import './style.css';
-import { taskList, addTask, clearCompleted } from './modules/add&remove.js';
+import {
+  taskList, addTask, clearCompleted, removeItem, Checkbox,
+} from './modules/ToDoList.js';
 
 window.addEventListener('load', () => {
   taskList();
   addTask();
+  taskList(Checkbox);
 });
 
 clearCompleted();
+taskList(removeItem);
--- src/modules/ToDoList.js
+++ src/modules/ToDoList.js
@@ -1,6 +1,6 @@
 let tasks = JSON.parse(localStorage.getItem('tasks')) || [];
 
-function saveTask() {
+export function saveTask() {
   localStorage.setItem('tasks', JSON.stringify(tasks));
 }
 
@@ -15,6 +15,18 @@ export function removeItem(listItem, index) {
   saveTask();
 }
 
+export function Checkbox(task, listItem) {
+  const checkbox = document.createElement('input');
+  checkbox.type = 'checkbox';
+  checkbox.classList.add('checkbox');
+  checkbox.checked = task.completed;
+  checkbox.addEventListener('change', () => {
+    task.completed = checkbox.checked;
+    saveTask();
+  });
+  listItem.appendChild(checkbox);
+}
+
 export function taskList() {
   const taskListElement = document.getElementById('todo-list');
   taskListElement.innerHTML = '';
@@ -23,20 +35,7 @@ export function taskList() {
     const listItem = document.createElement('li');
     listItem.classList.add('listItem');
 
-    const checkbox = document.createElement('input');
-    checkbox.type = 'checkbox';
-    checkbox.classList.add('checkbox');
-    checkbox.checked = task.completed;
-    checkbox.addEventListener('change', () => {
-      task.completed = checkbox.checked;
-      if (task.completed) {
-        listItem.classList.add('completed');
-      } else {
-        listItem.classList.remove('completed');
-      }
-      saveTask();
-    });
-    listItem.appendChild(checkbox);
+    Checkbox(task, listItem);
 
     const taskDiv = document.createElement('div');
     taskDiv.classList.add('taskDiv');
@@ -56,24 +55,22 @@ export function taskList() {
       });
     };
     taskDiv.addEventListener('dblclick', editDescription);
-
     listItem.appendChild(taskDiv);
 
     const dotsDiv = document.createElement('div');
     dotsDiv.classList.add('dotsDiv');
 
-    let dotsIcon = document.createElement('div');
-    dotsIcon = document.createElement('div');
+    const dotsIcon = document.createElement('div');
     dotsIcon.innerHTML = '<i class="bi bi-three-dots-vertical"></i>';
     dotsDiv.appendChild(dotsIcon);
-    dotsDiv.addEventListener('click', () => removeItem(listItem, index));
+    dotsIcon.addEventListener('click', () => removeItem(listItem, index));
     listItem.appendChild(dotsDiv);
     taskListElement.appendChild(listItem);
 
-    listItem.addEventListener('mouseover', () => {
-      dotsIcon.innerHTML = '<i class="bi bi-trash3-fill"></i>';
+    listItem.addEventListener('mouseenter', () => {
+      dotsIcon.innerHTML = '<i class="bi bi-trash3"></i>';
     });
-    listItem.addEventListener('mouseout', () => {
+    listItem.addEventListener('mouseleave', () => {
       dotsIcon.innerHTML = '<i class="bi bi-three-dots-vertical"></i>';
     });
   });
@@ -84,7 +81,7 @@ export function addTask() {
 
   function addNewTask() {
     if (!input.value) return;
-    const newTask = { description: input.value, completed: false, index: tasks.length };
+    const newTask = { description: input.value, completed: false, index: tasks.length + 1 };
     tasks.push(newTask);
     taskList();
     input.value = '';
